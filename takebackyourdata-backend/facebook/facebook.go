e44e7306a120d15fbb855c0b28940ea24100b04c
LukeLashley <conway220@gmail.com>
2021-06-24 16:17:23 -0400

    Commit to save

--- takebackyourdata-backend/facebook/facebook.go
+++ takebackyourdata-backend/facebook/facebook.go
@@ -1,18 +1,26 @@
 //Package facebook provides a holder for the facebook implementation
 package facebook
 
+import (
+	"fmt"
+	"log"
+)
+
 type facebookData struct {
 	ads facebookAds
 }
 
 //facebookAds struct holds all information relating to ad data on facebook
 type facebookAds struct {
-	numberOfAdvertisersConacts int
-	advertisersContactList     []string
+	numberOfAdvertisersContacts int
+	advertisersContactList      []string
 }
 
 // When you want a function to be visible to other classes, it does not follow camelcase
 func CreateData() *facebookData {
+	fmt.Println("test")
+	log.Println("test 2")
+
 	return &facebookData{}
 }
 
